4dbf630e94ba3274cc21a6579a2ac2cd71895fda
Hatsu <>
2024-06-07 18:33:02 +0800

    GameUI 繼續做到一半

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,15 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using SaintsField.Playa;
+using SaintsField;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
+    [BelowButton(nameof(Test_SetHpBarRatio_0))]
+    [BelowButton(nameof(Test_SetHpBarRatio_0p5))]
+    [BelowButton(nameof(Test_SetHpBarRatio_1))]
     [SerializeField] private Image hpBar;
+
+    [BelowButton(nameof(Test_SetScore_0))]
+    [BelowButton(nameof(Test_SetScore_50))]
+    [BelowButton(nameof(Test_SetScore_100))]
     [SerializeField] private TMP_Text scoreText;
+
+    [BelowButton(nameof(Test_ShowGameOver))]
+    [BelowButton(nameof(Test_HideGameOver))]
+    [SerializeField] private RectTransform gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +34,15 @@ public class GameUI : MonoBehaviour
     {
         if (tuple.id == nameof(Player) && tuple.eventType == EventType.Player && tuple.behaviorType == EventBehaviorType.HpChanged)
         {
-            var (hp, hpMax) = EventAggregator.Instance.InvokeRegisterEvent<(float hp, float hpMax)>(tuple.id, tuple.eventType, tuple.behaviorType);
-            var ratio = hp / hpMax;
-            SetHpBarRatio(ratio);
+            OnPlayerHpChanged(tuple);
         }
+    }
+
+    private void OnPlayerHpChanged((string id, EventType eventType, EventBehaviorType behaviorType) tuple)
+    {
+        var (hp, hpMax) = EventAggregator.Instance.InvokeRegisterEvent<(float hp, float hpMax)>(tuple.id, tuple.eventType, tuple.behaviorType);
+        var ratio = hp / hpMax;
+        SetHpBarRatio(ratio);
     }
 
     private void SetHpBarRatio(float value)
@@ -39,18 +55,22 @@ public class GameUI : MonoBehaviour
         scoreText.text = value.ToString();
     }
 
-    private void Test_SetHpBarRatio()
+    private void ShowGameOver()
     {
-
-    }
-    private void Test_SetScore()
-    {
-
+        gameOver.gameObject.SetActive(true);
     }
 
-    [Button]
-    private void OnButtonParams(UnityEngine.Object myObj, int myInt, string myStr = "hi")
+    private void HideGameOver()
     {
-        Debug.Log($"{myObj}, {myInt}, {myStr}");
+        gameOver.gameObject.SetActive(false);
     }
+
+    private void Test_SetHpBarRatio_0() => SetHpBarRatio(0);
+    private void Test_SetHpBarRatio_1() => SetHpBarRatio(1);
+    private void Test_SetHpBarRatio_0p5() => SetHpBarRatio(0.5f);
+    private void Test_SetScore_0() => SetScore(0);
+    private void Test_SetScore_50() => SetScore(50);
+    private void Test_SetScore_100() => SetScore(100);
+    private void Test_ShowGameOver() => ShowGameOver();
+    private void Test_HideGameOver() => HideGameOver();
 }
